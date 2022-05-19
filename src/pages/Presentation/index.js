df90e398e86d6d0cc34571d81218a78baeb3df83
akvh3 <andrewvon@gatech.edu>
2022-05-19 16:54:32 +0200

    changed bg, more

--- src/pages/Presentation/index.js
+++ src/pages/Presentation/index.js
@@ -44,19 +44,13 @@ import routes from "routes";
 import footerRoutes from "footer.routes";
 
 // Images
-import bgImage from "assets/images/bg-presentation.jpg";
+import bgImage from "assets/images/bg-presentation.png";
 
 function Presentation() {
   return (
     <>
       <DefaultNavbar
         routes={routes}
-        action={{
-          type: "external",
-          route: "https://www.creative-tim.com/product/material-kit-react",
-          label: "free download",
-          color: "info",
-        }}
         sticky
       />
       <MKBox
