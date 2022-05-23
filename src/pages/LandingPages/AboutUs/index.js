c00c61642340a71b336c62dbe2f6343edf274e01
akvh3 <andrewvon@gatech.edu>
2022-05-23 19:27:06 -0400

    finish about us page

--- src/pages/LandingPages/AboutUs/index.js
+++ src/pages/LandingPages/AboutUs/index.js
@@ -14,17 +14,13 @@ import Card from "@mui/material/Card";
 // Material Kit 2 React components
 import MKBox from "components/MKBox";
 import MKTypography from "components/MKTypography";
-import MKButton from "components/MKButton";
 
 // Material Kit 2 React examples
 import DefaultNavbar from "examples/Navbars/DefaultNavbar";
 import DefaultFooter from "examples/Footers/DefaultFooter";
 
 // About Us page sections
-import Information from "pages/LandingPages/AboutUs/sections/Information";
 import Team from "pages/LandingPages/AboutUs/sections/Team";
-import Featuring from "pages/LandingPages/AboutUs/sections/Featuring";
-import Newsletter from "pages/LandingPages/AboutUs/sections/Newsletter";
 
 // Routes
 import routes from "routes";
@@ -36,17 +32,7 @@ import bgImage from "assets/images/bg-about-us.jpg";
 function AboutUs() {
   return (
     <>
-      <DefaultNavbar
-        routes={routes}
-        action={{
-          type: "external",
-          route: "https://www.creative-tim.com/product/material-kit-react",
-          label: "free download",
-          color: "default",
-        }}
-        transparent
-        light
-      />
+      <DefaultNavbar routes={routes}/>
       <MKBox
         minHeight="75vh"
         width="100%"
@@ -64,50 +50,30 @@ function AboutUs() {
       >
         <Container>
           <Grid
-            container
-            item
-            xs={12}
-            lg={8}
-            justifyContent="center"
-            alignItems="center"
-            flexDirection="column"
-            sx={{ mx: "auto", textAlign: "center" }}
+              container
+              item
+              xs={12}
+              lg={8}
+              justifyContent="center"
+              alignItems="center"
+              flexDirection="column"
+              sx={{ mx: "auto", textAlign: "center" }}
           >
-            <MKTypography
+              <MKTypography
               variant="h1"
               color="white"
               sx={({ breakpoints, typography: { size } }) => ({
-                [breakpoints.down("md")]: {
+                  [breakpoints.down("md")]: {
                   fontSize: size["3xl"],
-                },
+                  },
               })}
-            >
-              Work with an amazing design
-            </MKTypography>
-            <MKTypography variant="body1" color="white" opacity={0.8} mt={1} mb={3}>
-              We&apos;re constantly trying to express ourselves and actualize our dreams. If you
-              have the opportunity to play this game
-            </MKTypography>
-            <MKButton color="default" sx={{ color: ({ palette: { dark } }) => dark.main }}>
-              create account
-            </MKButton>
-            <MKTypography variant="h6" color="white" mt={8} mb={1}>
-              Find us on
-            </MKTypography>
-            <MKBox display="flex" justifyContent="center" alignItems="center">
-              <MKTypography component="a" variant="body1" color="white" href="#" mr={3}>
-                <i className="fab fa-facebook" />
+              >
+              Learn about the boys in Snuggle Time.
               </MKTypography>
-              <MKTypography component="a" variant="body1" color="white" href="#" mr={3}>
-                <i className="fab fa-instagram" />
+              <MKTypography variant="body1" color="white" opacity={0.8} mt={1} mb={3}>
+              Read descriptions of each of our fabled members to get a glimpse into how our squad operates.
               </MKTypography>
-              <MKTypography component="a" variant="body1" color="white" href="#" mr={3}>
-                <i className="fab fa-twitter" />
-              </MKTypography>
-              <MKTypography component="a" variant="body1" color="white" href="#">
-                <i className="fab fa-google-plus" />
-              </MKTypography>
-            </MKBox>
+              
           </Grid>
         </Container>
       </MKBox>
@@ -120,10 +86,7 @@ function AboutUs() {
           boxShadow: ({ boxShadows: { xxl } }) => xxl,
         }}
       >
-        <Information />
         <Team />
-        <Featuring />
-        <Newsletter />
       </Card>
       <MKBox pt={6} px={1} mt={6}>
         <DefaultFooter content={footerRoutes} />
--- src/pages/LandingPages/AboutUs/sections/Team.js
+++ src/pages/LandingPages/AboutUs/sections/Team.js
@@ -25,10 +25,16 @@ import MKTypography from "components/MKTypography";
 import HorizontalTeamCard from "examples/Cards/TeamCards/HorizontalTeamCard";
 
 // Images
-import team1 from "assets/images/team-5.jpg";
-import team2 from "assets/images/bruce-mars.jpg";
-import team3 from "assets/images/ivana-squares.jpg";
-import team4 from "assets/images/ivana-square.jpg";
+import team1 from "assets/images/tyler.PNG";
+import team4 from "assets/images/nick.PNG";
+import team5 from "assets/images/andy.PNG";
+import team2 from "assets/images/joe.PNG";
+import team3 from "assets/images/matt.PNG";
+import team6 from "assets/images/sam.PNG";
+import team7 from "assets/images/eli.PNG";
+import team8 from "assets/images/mike.PNG";
+import team9 from "assets/images/ashton.PNG";
+import team10 from "assets/images/andrew.PNG";
 
 function Team() {
   return (
@@ -45,11 +51,7 @@ function Team() {
         <Grid container>
           <Grid item xs={12} md={8} sx={{ mb: 6 }}>
             <MKTypography variant="h3" color="white">
-              The Executive Team
-            </MKTypography>
-            <MKTypography variant="body2" color="white" opacity={0.8}>
-              There&apos;s nothing I really wanted to do in life that I wasn&apos;t able to get good
-              at. That&apos;s my skill.
+              The Snuggle Gang
             </MKTypography>
           </Grid>
         </Grid>
@@ -57,20 +59,30 @@ function Team() {
           <Grid item xs={12} lg={6}>
             <MKBox mb={1}>
               <HorizontalTeamCard
-                image={team1}
-                name="Emma Roberts"
-                position={{ color: "info", label: "UI Designer" }}
-                description="Artist is a term applied to a person who engages in an activity deemed to be an art."
+                image={team6}
+                name="Sam Bowers"
+                position={{ color: "info", label: "The Speedster" }}
+                description="Don't let his shyness deceive, he will run sub 4 in the mile and then steal your girl."
               />
             </MKBox>
           </Grid>
           <Grid item xs={12} lg={6}>
             <MKBox mb={1}>
               <HorizontalTeamCard
+                image={team1}
+                name="Tyler Bradshaw"
+                position={{ color: "info", label: "The Big Poop" }}
+                description="A legendary fibber, this man ran a sub-17 minute 5k as a pre-teen and once lost 12 pounds from one bowl movement."
+              />
+            </MKBox>
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <MKBox mb={{ xs: 1, lg: 0 }}>
+              <HorizontalTeamCard
                 image={team2}
-                name="William Pearce"
-                position={{ color: "info", label: "Boss" }}
-                description="Artist is a term applied to a person who engages in an activity deemed to be an art."
+                name="Joey Brovont"
+                position={{ color: "info", label: "The Heart" }}
+                description="If Snuggle Time has been silent for more than one day, be rest assured that this man will revive it as soon he notices."
               />
             </MKBox>
           </Grid>
@@ -78,9 +90,9 @@ function Team() {
             <MKBox mb={{ xs: 1, lg: 0 }}>
               <HorizontalTeamCard
                 image={team3}
-                name="Ivana Flow"
-                position={{ color: "info", label: "Athlete" }}
-                description="Artist is a term applied to a person who engages in an activity deemed to be an art."
+                name="Matt Brovont"
+                position={{ color: "info", label: "The DNer" }}
+                description="If this man ever asks you a question out of the blue, do NOT answer it or you will get dunked on."
               />
             </MKBox>
           </Grid>
@@ -88,9 +100,59 @@ function Team() {
             <MKBox mb={{ xs: 1, lg: 0 }}>
               <HorizontalTeamCard
                 image={team4}
-                name="Marquez Garcia"
-                position={{ color: "info", label: "JS Developer" }}
-                description="Artist is a term applied to a person who engages in an activity deemed to be an art."
+                name="Nick Butzow"
+                position={{ color: "info", label: "The Sneak" }}
+                description="This man will pass under the radar so elitely you might forget he's there unless you say Hey Nick."
+              />
+            </MKBox>
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <MKBox mb={{ xs: 1, lg: 0 }}>
+              <HorizontalTeamCard
+                image={team5}
+                name="Andy Butzow"
+                position={{ color: "info", label: "The Sex Appeal" }}
+                description="Without a doubt our most attractive member, this man makes every woman he walks by weak in the knees."
+              />
+            </MKBox>
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <MKBox mb={{ xs: 1, lg: 0 }}>
+              <HorizontalTeamCard
+                image={team7}
+                name="Eli Jones"
+                position={{ color: "info", label: "The Lube" }}
+                description="The jack of all trades with the most nicknames, he's the only member with frosted tips and has a smile that will melt your heart."
+              />
+            </MKBox>
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <MKBox mb={{ xs: 1, lg: 0 }}>
+              <HorizontalTeamCard
+                image={team8}
+                name="Michael McCarthy"
+                position={{ color: "info", label: "The Tank" }}
+                description="This man has broken more bones than you, and also got hit by a car and went to Africa two days later. Beast."
+              />
+            </MKBox>
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <MKBox mb={{ xs: 1, lg: 0 }}>
+              <HorizontalTeamCard
+                image={team9}
+                name="Ashton Meade"
+                position={{ color: "info", label: "The Dancer" }}
+                description="I didn't forget all the dances Ashton used to post on Instagram, and I won't let you forget either."
+              />
+            </MKBox>
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <MKBox mb={{ xs: 1, lg: 0 }}>
+              <HorizontalTeamCard
+                image={team10}
+                name="Andrew von Hillebrandt"
+                position={{ color: "info", label: "The Historian" }}
+                description="I have 97 GBs of data on my phone and about 90 of them are photos and old texts, a.k.a. I have a hoarding problem."
               />
             </MKBox>
           </Grid>
